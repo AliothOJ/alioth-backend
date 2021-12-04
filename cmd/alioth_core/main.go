69ffb644f15d4881c820a80da5f501663e1125ad
jecosine <jecosine@gmail.com>
2021-12-04 19:07:35 +0800

    UPDATE: Add codes to test config reader

--- cmd/alioth_core/main.go
+++ cmd/alioth_core/main.go
@@ -1,1 +1,37 @@
-package alioth_core
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/AliothOJ/backend/internal/config"
+	"log"
+)
+
+type LaunchOption struct {
+	ConfPath string
+}
+
+func ParseFlags() (*LaunchOption, error) {
+	option := new(LaunchOption)
+	flag.StringVar(&(option.ConfPath), "config", "../../config.yaml", "path to yaml file")
+	flag.Parse()
+	if err:=config.CheckExists(option.ConfPath); err != nil {
+		return nil, err
+	}
+	return option, nil
+}
+
+func main() {
+	option, err :=  ParseFlags()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config, err := config.LoadConfig(option.ConfPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// test print config
+	fmt.Println(Config.Server.Port)
+	fmt.Println(Config.Microservices[0].Slaves[0].GRPCPort)
+
+}
